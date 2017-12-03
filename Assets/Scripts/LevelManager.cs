fe2186cc831edd819cadf0cfcc204fcc1df17053
Hugo-C <hchastel@laposte.net>
2017-12-03 14:01:55 +0100

    ajout d'un background et de setting pour lancer en mode fenetré, + animation plus fluide

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape)){
+            Application.Quit();
+        }
+    }
+
+    // DEBUG
+    void OnApplicationQuit() {
+        PlayerPrefs.SetInt("Screenmanager Resolution Width", 800);
+        PlayerPrefs.SetInt("Screenmanager Resolution Height", 600);
+        PlayerPrefs.SetInt("Screenmanager Is Fullscreen mode", 0);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour {
     const int WALK_BACK = 3;
     const int WALK_LEFT = 4;
 
+    const float WALK_STEEP = 0.1f;
+
     // Use this for initialization
     void Start () {
 		
@@ -19,14 +21,26 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Animator a = gameObject.GetComponent<Animator>();
-        if (Input.GetKey(KeyCode.Z)) {
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow)) {
             a.SetInteger("walk", WALK_FRONT);
-        } else if (Input.GetKey(KeyCode.D)) {
+            Vector3 newPosition = gameObject.transform.position;
+            newPosition.y += WALK_STEEP;
+            gameObject.transform.position = newPosition;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
             a.SetInteger("walk", WALK_RIGHT);
-        } else if (Input.GetKey(KeyCode.S)) {
+            Vector3 newPosition = gameObject.transform.position;
+            newPosition.x += WALK_STEEP;
+            gameObject.transform.position = newPosition;
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
             a.SetInteger("walk", WALK_BACK);
-        } else if (Input.GetKey(KeyCode.Q)) {
+            Vector3 newPosition = gameObject.transform.position;
+            newPosition.y -= WALK_STEEP;
+            gameObject.transform.position = newPosition;
+        } else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow)) {
             a.SetInteger("walk", WALK_LEFT);
+            Vector3 newPosition = gameObject.transform.position;
+            newPosition.x -= WALK_STEEP;
+            gameObject.transform.position = newPosition;
         } else {
             a.SetInteger("walk", IDLE);
         }
