09b8ffc17851837e0ffadd0891f443fc869801ec
Hugo-C <hchastel@laposte.net>
2017-12-26 21:58:56 +0100

    avancement des mouvements du joueur
    avec un double linecasts, detecte les gameobjects qui possedent le script wall

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.00001f;
+    public float moveCoef = 0.00001f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private Vector2 shift = new Vector2(0f, 0.33f);
+    private float inverseMoveTime;
+
+    // Use this for initialization
+    protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit1, out RaycastHit2D hit2) {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2((float)xDir * moveCoef * 0.25f, (float)yDir * moveCoef * 0.25f);  // TODO nettoyer tous ça
+
+        boxCollider.enabled = false;  // we don't want to hit our own boxCollider
+        hit1 = Physics2D.Linecast(start + shift, end + shift, blockingLayer);  // first hit a bit above
+        hit2 = Physics2D.Linecast(start - shift, end - shift, blockingLayer);  // second hit a bit under
+        boxCollider.enabled = true;
+        if(hit1.transform == null && hit2.transform == null) {
+            gameObject.transform.position = end;
+            SmoothMovement(end);
+            return true;
+        }
+        return false;
+    }
+
+    protected void SmoothMovement(Vector3 end) {
+        Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+        rb2D.MovePosition(newPosition);
+    }
+
+
+    protected virtual void AttemptMove <T> (int xDir, int yDir)
+        where T : Component {
+        RaycastHit2D hit1;
+        RaycastHit2D hit2;
+        bool canMove = Move(xDir, yDir, out hit1, out hit2);
+
+        if(hit1.transform == null && hit2.transform == null) {
+            return;
+        }
+
+        T hitComponent1 = hit1.transform.GetComponent<T>();
+        T hitComponent2 = hit2.transform.GetComponent<T>();
+        if (!canMove && ((hitComponent1 != null) || (hitComponent1 != null))) {
+            T hitComponent = hitComponent1;
+            if (hitComponent == null) {
+                hitComponent = hitComponent2;
+            }
+            OnCantMove(hitComponent);
+        }
+    }
+
+    protected abstract void OnCantMove <T> (T component)
+        where T : Component;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,48 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MovingObject {
 
 
-    const int IDLE = 0;
-    const int WALK_FRONT = 1;
-    const int WALK_RIGHT = 2;
-    const int WALK_BACK = 3;
-    const int WALK_LEFT = 4;
+    const float STEEP = 0.5f;
 
-    const float WALK_STEEP = 0.1f;
+    const int ANIM_IDLE = 0;
+    const int ANIM_WALK_FRONT = 1;
+    const int ANIM_WALK_RIGHT = 2;
+    const int ANIM_WALK_BACK = 3;
+    const int ANIM_WALK_LEFT = 4;
+
+    private Animator animator;
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    protected override void Start () {
+        animator = gameObject.GetComponent<Animator>();
+        base.Start();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Animator a = gameObject.GetComponent<Animator>();
-        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow)) {
-            a.SetInteger("walk", WALK_FRONT);
-            Vector3 newPosition = gameObject.transform.position;
-            newPosition.y += WALK_STEEP;
-            gameObject.transform.position = newPosition;
-        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            a.SetInteger("walk", WALK_RIGHT);
-            Vector3 newPosition = gameObject.transform.position;
-            newPosition.x += WALK_STEEP;
-            gameObject.transform.position = newPosition;
-        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            a.SetInteger("walk", WALK_BACK);
-            Vector3 newPosition = gameObject.transform.position;
-            newPosition.y -= WALK_STEEP;
-            gameObject.transform.position = newPosition;
-        } else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow)) {
-            a.SetInteger("walk", WALK_LEFT);
-            Vector3 newPosition = gameObject.transform.position;
-            newPosition.x -= WALK_STEEP;
-            gameObject.transform.position = newPosition;
-        } else {
-            a.SetInteger("walk", IDLE);
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int) (Input.GetAxisRaw("Horizontal"));
+        vertical = (int) (Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0) {
+            vertical = 0;  // the player can't move diagnoly (for now at least)
+        }
+
+        HandleAnimation(horizontal, vertical);
+        if (horizontal != 0 || vertical != 0) {
+            AttemptMove<Wall>(horizontal, vertical);
         }
 	}
+
+    private void HandleAnimation(int horizontal, int vertical) {
+        if (vertical == 0 && horizontal == 0) {
+            animator.SetInteger("walk", ANIM_IDLE); // we don't have yet an idle animation
+        } else if (vertical > 0) {
+            animator.SetInteger("walk", ANIM_WALK_FRONT);
+        } else if (horizontal > 0) {
+            animator.SetInteger("walk", ANIM_WALK_RIGHT);
+        } else if (vertical < 0) {
+            animator.SetInteger("walk", ANIM_WALK_BACK);
+        } else if (horizontal < 0) {
+            animator.SetInteger("walk", ANIM_WALK_LEFT);
+        } else {
+            Debug.Log("error in HandleAnimation : InvalidValue");
+        }
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir) {
+        RaycastHit2D hit1;
+        RaycastHit2D hit2;
+        if (Move(xDir, yDir, out hit1, out hit2)) {
+            //Debug.Log("i moved");
+        }
+    }
+
+    protected override void OnCantMove<T>(T component) {
+        Debug.Log("I'm blocked !");
+    }
+    
 }
