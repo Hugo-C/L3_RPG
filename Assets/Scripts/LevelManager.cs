7c1b9fc9cb1a0131fe38fbd845470a3a79b97abf
Hugo-C <hchastel@laposte.net>
2018-06-05 23:16:07 +0200

    Ajout de coins à la salle généré
    ajout du mur sud

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-    const int MIN_SIZE_ROOM = 15;
+    const int MIN_SIZE_ROOM = 10;
     const int MAX_SIZE_ROOM = 20;
-    public static LevelManager instance = null;
+    public static LevelManager instance = null;  // singleton patern
     public GameObject floor;
     public GameObject northWall;
+    public GameObject southWall;
+    public GameObject cornerUpperLeft;
+    public GameObject cornerUpperRight;
+    public GameObject cornerBottomRight;
+    public GameObject cornerBottomLeft;
 
     //Awake is always called before any Start functions
     void Awake() {
@@ -48,32 +53,50 @@ public class LevelManager : MonoBehaviour {
 
         // generate walls
         GameObject myNorthWall = Instantiate(northWall, new Vector3(0f, (floorHeight + northWall.GetComponent<SpriteRenderer>().size.y) / 2), Quaternion.identity);
+        myNorthWall.name = "myNorthWall";
         SpriteRenderer srNorthWall = myNorthWall.GetComponent<SpriteRenderer>();
         srNorthWall.drawMode = SpriteDrawMode.Tiled;
-        srNorthWall.size = new Vector2(floorWidth / 1.9f, srNorthWall.size.y);
+        srNorthWall.size = new Vector2(floorWidth / 1.9f - srNorthWall.size.y, srNorthWall.size.y);
         BoxCollider2D myNorthbc = myNorthWall.GetComponent<BoxCollider2D>();
-        myNorthbc.size = new Vector2(floorWidth / 1.9f, myNorthbc.size.y);
+        myNorthbc.size = new Vector2(floorWidth / 1.9f - myNorthbc.size.y, myNorthbc.size.y);
 
         GameObject myEastWall = Instantiate(northWall, new Vector3((floorWidth + northWall.GetComponent<SpriteRenderer>().size.y) / 2, 0f), Quaternion.Euler(0f, 0f, -90f));
+        myEastWall.name = "myEastWall";
         SpriteRenderer srEastWall = myEastWall.GetComponent<SpriteRenderer>();
         srEastWall.drawMode = SpriteDrawMode.Tiled;
-        srEastWall.size = new Vector2(floorHeight / 1.9f, srEastWall.size.y);
+        srEastWall.size = new Vector2(floorHeight / 1.9f - srEastWall.size.y, srEastWall.size.y);
         BoxCollider2D myEasthbc = myEastWall.GetComponent<BoxCollider2D>();
-        myEasthbc.size = new Vector2(floorHeight / 1.9f, myEasthbc.size.y);
+        myEasthbc.size = new Vector2(floorHeight / 1.9f - myEasthbc.size.y, myEasthbc.size.y);
 
-        GameObject mySouthWall = Instantiate(northWall, new Vector3(0f, - (floorHeight + northWall.GetComponent<SpriteRenderer>().size.y) / 2), Quaternion.Euler(0f, 0f, 180f));
+        GameObject mySouthWall = Instantiate(southWall, new Vector3(0f, - (floorHeight + northWall.GetComponent<SpriteRenderer>().size.y) / 2), Quaternion.identity);
+        mySouthWall.name = "mySouthWall";
         SpriteRenderer srSouthtWall = mySouthWall.GetComponent<SpriteRenderer>();
         srSouthtWall.drawMode = SpriteDrawMode.Tiled;
-        srSouthtWall.size = new Vector2(floorWidth / 1.9f, srSouthtWall.size.y);
+        srSouthtWall.size = new Vector2(floorWidth / 1.9f - srSouthtWall.size.y, srSouthtWall.size.y);
         BoxCollider2D mySouthbc = mySouthWall.GetComponent<BoxCollider2D>();
-        mySouthbc.size = new Vector2(floorWidth / 1.9f, mySouthbc.size.y);
+        mySouthbc.size = new Vector2(floorWidth / 1.9f - mySouthbc.size.y, mySouthbc.size.y);
 
         GameObject myWestWall = Instantiate(northWall, new Vector3(- (floorWidth + northWall.GetComponent<SpriteRenderer>().size.y) / 2, 0f), Quaternion.Euler(0f, 0f, 90f));
+        myWestWall.name = "myWestWall";
         SpriteRenderer srWestWall = myWestWall.GetComponent<SpriteRenderer>();
         srWestWall.drawMode = SpriteDrawMode.Tiled;
-        srWestWall.size = new Vector2(floorHeight / 1.9f, srWestWall.size.y);
+        srWestWall.size = new Vector2(floorHeight / 1.9f - srWestWall.size.y, srWestWall.size.y);
         BoxCollider2D myWestbc = myWestWall.GetComponent<BoxCollider2D>();
-        myWestbc.size = new Vector2(floorHeight / 1.9f, myWestbc.size.y);
+        myWestbc.size = new Vector2(floorHeight / 1.9f - myWestbc.size.y, myWestbc.size.y);
+
+        // add corners to walls
+        Vector3 cornerUpperLeftPos = new Vector3(myWestWall.transform.position.x, myNorthWall.transform.position.y, -1);
+        GameObject myCornerUpperLeft = Instantiate(cornerUpperLeft, cornerUpperLeftPos, Quaternion.identity);
+
+        Vector3 cornerUpperRightPos = new Vector3(myEastWall.transform.position.x, myNorthWall.transform.position.y, -1);
+        GameObject myCornerUpperRight = Instantiate(cornerUpperRight, cornerUpperRightPos, Quaternion.identity);
+
+        Vector3 cornerBottomRightPos = new Vector3(myEastWall.transform.position.x, mySouthWall.transform.position.y, -1);
+        GameObject myCornerBottomRight = Instantiate(cornerBottomRight, cornerBottomRightPos, Quaternion.identity);
+
+        Vector3 cornerBottomLeftPos = new Vector3(myWestWall.transform.position.x, mySouthWall.transform.position.y, -1);
+        GameObject myCornerBottomLeft = Instantiate(cornerBottomLeft, cornerBottomLeftPos, Quaternion.identity);
+
     }
 
     // DEBUG
